72239886e8de5cc260e9735f3114e66d7b9852f7
Juan Fajardo <juanfar.group@gmail.com>
2024-01-13 18:21:07 +0100

    feat: Enabling 'api' global prefix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { VersioningType } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
   app.enableVersioning({
     type: VersioningType.URI,
   });
