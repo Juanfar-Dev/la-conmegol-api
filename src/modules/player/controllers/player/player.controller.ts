fd902532a98fab834dfb5f0c60d7c94660404530
Juan Fajardo <juanfar.group@gmail.com>
2024-01-13 18:17:08 +0100

    feat: Enabling api versioning

--- src/modules/player/controllers/player/player.controller.ts
+++ src/modules/player/controllers/player/player.controller.ts
@@ -6,6 +6,7 @@ import {
   Param,
   Post,
   Put,
+  Version,
 } from '@nestjs/common';
 import { PlayerService } from '../../services/player/player.service';
 import { Player } from '@prisma/client';
@@ -14,26 +15,31 @@ import { Player } from '@prisma/client';
 export class PlayerController {
   constructor(private readonly playerService: PlayerService) {}
 
+  @Version('1')
   @Get()
   getAllPlayers() {
     return this.playerService.getAllPlayers();
   }
 
+  @Version('1')
   @Get(':id')
   async getPlayerByID(@Param('id') id: string) {
     return this.playerService.getPlayerByID(Number(id));
   }
 
+  @Version('1')
   @Post()
   async createPlayer(@Body() data: Player) {
     return this.playerService.createPlayer(data);
   }
 
+  @Version('1')
   @Put(':id')
   async updatePlayers(@Param('id') id: string, @Body() data: Player) {
     return this.playerService.updatePlayers(Number(id), data);
   }
 
+  @Version('1')
   @Delete(':id')
   async deletePlayer(@Param('id') id: string) {
     return this.playerService.deletePlayer(Number(id));
